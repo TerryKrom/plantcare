fb2b7882ec3c7b7bbc6d8a76c17b40011f9f53fa
TerryKrom <terrykrom1@gmail.com>
2023-09-30 19:38:46 -0300

    wiki updated

--- wiki.mjs
+++ wiki.mjs
@@ -10,49 +10,49 @@ const flower = createObject(
     './icons/flower.png',
     ['Orchid', 'Rose', 'Daisy', 'Sunflower'],
     'A beautiful type of plant!',
-    'They need water, sunlight, and proper care to bloom.'
+    'Flowers require regular watering, typically once a week. They thrive in bright, indirect sunlight and prefer moderate humidity. Fertilize them during the growing season for best results.'
 );
 
 const fruitplant = createObject(
     './icons/fruit.png',
     ['Apple', 'Pear', 'Banana', 'Orange'],
     'Delicious fruit-bearing plants!',
-    'They need water, sunlight, and regular pruning for healthy fruit production.'
+    'Fruit plants need consistent watering, especially during the fruiting season. They thrive in full sunlight and benefit from regular pruning to encourage healthy fruit production. Mulching can help retain soil moisture.'
 );
 
 const tree = createObject(
     './icons/tree.png',
     ['Oak', 'Pine', 'Maple', 'Palm'],
     'Tall and woody plants!',
-    'They provide shade and require occasional trimming and maintenance. Take a lot time to a tree to grow'
+    'Trees require deep, infrequent watering to establish deep roots. They prefer full sunlight and should be pruned periodically to remove dead branches. Protect them from pests and diseases for long-term health.'
 );
 
 const cactus = createObject(
     './icons/cactus.png',
     ['Saguaro Cactus', 'Barrel Cactus', 'Opuntia', 'Echinopsis'],
     'Hardy desert plants!',
-    'They thrive in arid conditions and require very little water.'
+    'Cacti thrive in arid conditions and need infrequent watering, typically every 2-4 weeks. They require full sunlight and well-draining soil. Avoid overwatering, as it can lead to root rot.'
 );
 
 const herbs = createObject(
     './icons/herbs.png',
     ['Basil', 'Parsley', 'Mint', 'Thyme'],
     'Aromatic and culinary plants!',
-    'They are great for cooking and need well-drained soil and sunlight.'
+    'Herbs prefer consistent moisture, so water them when the soil surface feels dry. They thrive in sunlight, and you can harvest their leaves for culinary use. Pruning helps maintain their shape and flavor.'
 );
 
 const tuber = createObject(
     './icons/tuber.png',
     ['Potato', 'Carrot', 'Beet', 'Yam'],
     'Plants with edible underground parts!',
-    'They need loose soil and consistent watering for optimal tuber growth.'
+    'Tuber plants need well-draining soil and regular watering to ensure proper tuber development. They prefer full sunlight and benefit from mulching to conserve soil moisture.'
 );
 
 const grains = createObject(
     './icons/grains.png',
     ['Wheat', 'Corn', 'Rice', 'Oats'],
     'Crops grown for their grains!',
-    'They require fertile soil, adequate irrigation, and careful cultivation.'
+    'Grain crops require fertile soil and consistent irrigation, especially during their growth stages. They benefit from full sunlight and may need pest control measures to protect the harvest.'
 );
 
 export { flower, fruitplant, tree, cactus, herbs, tuber, grains };
